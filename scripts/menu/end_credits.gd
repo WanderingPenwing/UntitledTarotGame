de170ca18a2f49ec76cdd066d4783a17b6603099
Kamille-is-a-mans-name <powerduff0@gmail.com>
2025-09-21 19:11:41 +0200

    typo cutscene 7 and 8 and credits edit

--- scripts/menu/end_credits.gd
+++ scripts/menu/end_credits.gd
@@ -6,3 +6,7 @@ extends Node2D
 func _ready() -> void:
 	var tween = get_tree().create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	tween.tween_property(text, "position", Vector2(0,-450.0), 60)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("B"):
+		get_tree().change_scene_to_file("res://prefabs/menu/start_menu.tscn")
